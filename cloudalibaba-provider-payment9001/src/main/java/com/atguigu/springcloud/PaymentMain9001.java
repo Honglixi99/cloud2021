c718363ca35729d9e8713faaa6ad6c703e98f857
honglixi <boy2314@foxmail.com>
2021-04-19 09:16:27 +0800

    SpringCloud

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Honglixi
+ * @create 2021-04-18-14:20
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+/**
+ * @author Honglixi
+ * @create 2021-04-16-23:04
+ */
+public class Hello {
+
+}
